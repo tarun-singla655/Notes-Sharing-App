997ca0ab946ff8fb8482d717f2761a72509e4918
dipesh809 <b20096@students.iitmandi.ac.in>
2022-02-02 16:21:03 +0530

    added login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,7 +3,52 @@ import React from 'react'
 export default function Login() {
     return (
         <div>
-            
+            <section className="vh-100">
+                <div className="container-fluid h-custom">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col-md-9 col-lg-6 col-xl-5">
+                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid"
+                                alt="Sample image" />
+                        </div>
+                        <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                            <form>
+                                <div className="divider d-flex align-items-center my-4">
+                                <p className="lead fw-normal mb-0 me-3">Sign-in</p>
+                                </div>
+                                <div className="form-outline mb-4">
+                                    <label className="form-label" htmlFor="form3Example3">Email address</label>
+                                    <input type="email" id="form3Example3" className="form-control form-control-lg"
+                                        placeholder="Enter a valid email address" />
+                                </div>
+
+                                <div className="form-outline mb-3">
+                                    <label className="form-label" htmlFor="form3Example4">Password</label>
+                                    <input type="password" id="form3Example4" className="form-control form-control-lg"
+                                        placeholder="Enter password" />
+                                </div>
+
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <div className="form-check mb-0">
+                                        <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                                        <label className="form-check-label" htmlFor="form2Example3">
+                                            Remember me
+                                        </label>
+                                    </div>
+                                    <a href="#!" className="text-body">Forgot password?</a>
+                                </div>
+
+                                <div className="text-center text-lg-start mt-4 pt-2">
+                                    <button type="button" className="btn btn-primary btn-lg"
+                                        style={ {"padding-left": "2.5rem", "padding-right": "2.5rem","textAlign":"center"}}>Login</button>
+                                    <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
+                                        className="link-danger">Register</a></p>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
